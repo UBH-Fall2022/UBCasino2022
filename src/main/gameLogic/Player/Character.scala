1df0f48a5db19526565b88f8be62dcae3b7deb64
Journey <journeys@buffalo.edu>
2022-11-06 02:38:37 -0500

    11/6 238 am

--- src/main/gameLogic/Player/Character.scala
+++ src/main/gameLogic/Player/Character.scala
@@ -10,6 +10,22 @@ abstract class Character {
   var winnerWeight: Int = 0
   var straightBy: Int = 0
   var in: Boolean = false
+  
+  def displayCards(): Unit = {
+    for (ele <- privHand) {
+      if (ele.cardValue == 11) {
+        println("Jack of " + ele.cardSuit)
+      } else if(ele.cardValue == 12) {
+        println("Queen of " + ele.cardSuit)
+      } else if (ele.cardValue == 13) {
+        println("King of " + ele.cardSuit)
+      } else if (ele.cardValue == 14) {
+        println("Ace of " + ele.cardSuit)
+      } else {
+        println(ele.cardValue + " of " + ele.cardSuit)
+      }
+    }
+  }
 
   def changePrice(balance: Int): Unit = {
     this.balance = balance
