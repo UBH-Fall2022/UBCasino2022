8e32bc3fead4ecf2e44189d580130ceccc4f9242
Saucy1211 <elijahll@buffalo.edu>
2022-11-05 20:38:01 -0400

    GUI

---
+++ src/main/GUI/GUI.scala
@@ -0,0 +1,17 @@
+package main.GUI
+import javafx.scene.input.MouseEvent
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.control.TextField
+import javafx.event.ActionEvent
+import javafx.application.Application
+import javafx.event.EventHandler
+import javafx.scene.control.Button
+import javafx.scene.layout.StackPane
+
+
+class GUI {
+
+
+}
